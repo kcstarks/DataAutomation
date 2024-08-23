30afc5eb6fc4d4c36ff80f49330863e13de411fb
Kason Starks <104486355+kcstarks@users.noreply.github.com>
2024-08-23 13:06:15 -0500

    Updated cleaner
    Added samsara files to cleaner

--- cleaner.py
+++ cleaner.py
@@ -8,14 +8,18 @@ def cleaner():
         'C:/Users/Dispatch2/Desktop/DataAutomation/reports/daily/drivers/driver_report.csv',
         'C:/Users/Dispatch2/Desktop/DataAutomation/reports/daily/orders/order_report.csv',
         'C:/Users/Dispatch2/Desktop/DataAutomation/reports/daily/daily_report.xlsx',
+        'C:/Users/Dispatch2/Desktop/DataAutomation/reports/daily/samsara/safety_report.csv'
+
 
         'C:/Users/Dispatch2/Desktop/DataAutomation/reports/weekly/drivers/driver_report.csv',
         'C:/Users/Dispatch2/Desktop/DataAutomation/reports/weekly/orders/order_report.csv',
         'C:/Users/Dispatch2/Desktop/DataAutomation/reports/weekly/weekly_report.xlsx',
+        'C:/Users/Dispatch2/Desktop/DataAutomation/reports/weekly/samsara/safety_report.csv'
 
         'C:/Users/Dispatch2/Desktop/DataAutomation/reports/monthly/drivers/driver_report.csv',
         'C:/Users/Dispatch2/Desktop/DataAutomation/reports/monthly/orders/order_report.csv',
         'C:/Users/Dispatch2/Desktop/DataAutomation/reports/monthly/monthly_report.csv',
+        'C:/Users/Dispatch2/Desktop/DataAutomation/reports/monthly/samsara/safety_report.csv'
     ]
    
     for file in folder_dictionary:
