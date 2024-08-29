cc7d25622bcf84e5fc04e8aa30886be1ec300f14
Kason Starks <104486355+kcstarks@users.noreply.github.com>
2024-08-29 15:20:40 -0500

    Update: reoccurring automation
    Added functionality to reoccurring and its analysis file pd_reoccurring

--- pd_reoccurring.py
+++ pd_reoccurring.py
@@ -1,37 +1,45 @@
 import pandas as pd
 import datetime as dt
 
+class ReturnObject:
+    def __init__(self, date_range, table):
+        self.date_range = date_range,
+        self.table = table
 
+class Reoccurring_Analysis:
+    def __init__(self):
+        df = pd.DataFrame(pd.read_csv('C:/Users/Dispatch2/Desktop/DataAutomation/reoccurring/reoccurring_transports.csv', skiprows=1))
+        date_range = pd.DataFrame(pd.read_csv('C:/Users/Dispatch2/Desktop/DataAutomation/reoccurring/reoccurring_transports.csv', nrows=1))
 
-#class Reoccurring_Analysis:
-#    def __init__(self, object):
-df = pd.DataFrame(pd.read_csv('C:/Users/Dispatch2/Desktop/DataAutomation/reoccurring/reoccurring_transports.xlsx', skiprows=1))
-date_range = pd.DataFrame(pd.read_csv('C:/Users/Dispatch2/Desktop/DataAutomation/reoccurring/reoccurring_transports.xlsx', nrows=1))
+        df_no_nans = df.dropna(axis=0) 
+        df_unique_only = df_no_nans.drop_duplicates()
+        df_unique_only = df_unique_only.rename(columns={'Passenger\'s First Name': 'first', 'Passenger\'s Last Name': 'last', 'Standing Order Start Date': 'start', 'Standing Order End Date': 'end'})
+        df_unique_only.reset_index(drop=True, inplace=True)
+        df_unique_only['end'] = pd.to_datetime(df_unique_only['end'], format='%Y-%m-%d')
+        df_unique_only['end'] = df_unique_only['end'].apply(lambda x: x.date())
 
-df_no_nans = df.dropna(axis=0) 
-df_unique_only = df_no_nans.drop_duplicates()
-df_unique_only.rename(columns={'Passenger\'s First Name': 'first', 'Passenger\'s Last Name': 'last', 'Standing Order Start Date': 'start', 'Standing Order End Date': 'end'}, inplace=True)
-df_unique_only['end'] = pd.to_datetime(df_unique_only['end']).apply(lambda x: x.date())
-#df_unique_only['end'] = df_unique_only['end'].apply(dt.date.)
+        current_date = dt.date.today()
 
-df_unique_only.reset_index(inplace=True, drop=True)
-print(df_unique_only.head(20))
-current_date = dt.date.today()
+        df_ending_soon = pd.DataFrame(columns=['First Name', 'Last Name', 'End Date'])
 
-#for row in df_unique_only:
-#    if current_date - df_unique_only['end'] < pd.Timedelta('30 days'):
-#        print(df_unique_only.iloc[row][['first', 'last', 'end']])
-#    else:
-#        pass
-#print(df_unique_only.iloc[6]['end'] - current_date)
-    
+        for i in range(1, len(df_unique_only)):
+            if df_unique_only.iloc[i]['end'] - current_date > pd.Timedelta('31 days'):
+                pass
+            elif df_unique_only.iloc[i]['end'] - current_date < pd.Timedelta('0 days'):
+                pass
+            else:
+                row_to_add = pd.DataFrame(
+                    [{
+                        'First Name': df_unique_only.iloc[i]['first'], 
+                        'Last Name': df_unique_only.iloc[i]['last'], 
+                        'End Date': df_unique_only.iloc[i]['end']
+                    }]
+                )
+                df_ending_soon = pd.concat([df_ending_soon, row_to_add])
+
+        df_ending_soon.reset_index(drop=True, inplace=True)
         
-
-#for i in range(1, len(df_unique_only)):
-#    if df_unique_only.iloc[i]['end'] - current_date < pd.Timedelta('31 days'):
-#        print(df_unique_only[i][['first', 'last', 'end']])
-#    else:
-#        pass
-
-#print(df_unique_only.head(20))
-        
+        self.data = ReturnObject(
+            table = df_ending_soon, 
+            date_range = date_range
+        )
--- reoccurring.py
+++ reoccurring.py
@@ -1,8 +1,13 @@
 #This file is for alerting users to reoccurring transports that are expiring soon.
 
 import configparser
+import shutil
 from msgraph.generated.models.o_data_errors.o_data_error import ODataError
 import asyncio
+
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment 
+import pandas as pd
 from graph import Graph
 from main import get_attachment, get_attachment_id, get_inboxes, get_message_id
 from pd_reoccurring import Reoccurring_Analysis
@@ -26,27 +31,74 @@ async def reoccurring():
         if odata_error.error:
             print(odata_error.error.code, odata_error.error.message)
 
-class Upload_File:
-    def __init__(self, file:str) -> None:
-        self.csv = file
 
+#------------------------------------------------------
 
 async def create_reoccurring(graph):
     reoccurring = Reoccurring_Transports(graph)
     await reoccurring._init(graph)
     return reoccurring
 
+def export_excel(file_path, table):
+    template = 'C:/Users/Dispatch2/Desktop/DataAutomation/reports/blank.xlsx'
+
+    shutil.copyfile(template, file_path)
+
+    with pd.ExcelWriter(
+        file_path,
+        mode='a',
+        engine='openpyxl',
+        if_sheet_exists='overlay'
+    ) as writer:
+        table.to_excel(writer, sheet_name='Sheet1', startrow=0, startcol=0, index=False)
+
+
+    wb = load_workbook(file_path)
+    ws = wb.active
+    
+    for column in ws.columns:
+        max_length = 0
+        column_letter = column[0].column_letter
+
+
+        for cell in column:
+            try: 
+                if len(str(cell.value)) > max_length:
+                    max_length = len(str(cell.value))
+            except:
+                print('export_excel() error: cell width error')
+
+        adjusted_width = (max_length + 2) * 1.2
+        ws.column_dimensions[column_letter].width = adjusted_width
+
+        for cell in column:
+            try:
+                cell.alignment = Alignment(horizontal='center')
+            except:
+                print('export_excel() error: cell alignment error')
+    
+    
+    wb.save(file_path)
+    print('Data exported to excel file')
+   
+
+
 
 class Reoccurring_Transports:
     
     def __init__(self, graph:Graph):
-        self.reoccurring_file = 'C:/Users/Dispatch2/Desktop/DataAutomation/reoccurring/reoccurring_transports.xlsx'
+        self.reoccurring_file_csv = 'C:/Users/Dispatch2/Desktop/DataAutomation/reoccurring/reoccurring_transports.csv'
+        self.reoccurring_file_excel = 'C:/Users/Dispatch2/Desktop/DataAutomation/reoccurring/reoccurring_transports.xlsx'
 
     async def _init(self, graph:Graph):
         print('...')
         await self.mk_reoccurring_csv(graph)
-        analysis_obj = Upload_File(self.reoccurring_file)
-        analysis_result = Reoccurring_Analysis(analysis_obj)
+        analysis_result = Reoccurring_Analysis()
+        data = analysis_result.data
+        table = data.table
+        date = data.date_range
+
+        export_excel(self.reoccurring_file_excel, table)
 
     async def mk_reoccurring_csv(self, graph:Graph):
         folder = 'reoccurring'
@@ -54,7 +106,7 @@ class Reoccurring_Transports:
         attachment_id = await get_attachment_id(graph, message_id, folder)
         csv = await get_attachment(graph, message_id, attachment_id, folder)
         
-        print(csv, file=open(self.reoccurring_file, 'w', newline='\n'))
+        print(csv, file=open(self.reoccurring_file_csv, 'w', newline='\n'))
 
  
 asyncio.run(reoccurring())
